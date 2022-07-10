8bcd88b6c4dd6ca952ba3bd9777fd9bedf5b1ad6
thiagoss2000 <thiago.ss675@gmail.com>
2022-07-10 14:36:09 -0300

    bug-fix

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, { json } from "express";
+import "express-async-errors";
+import dotenv from "dotenv";
+import router from "./src/routes/router.js";
+import errorHandler from "./src/middlewares/errorHandlerMiddleware.js";
+import chalk from "chalk";
+dotenv.config();
+
+const app = express();
+app.use(json());
+
+app.use(router);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(chalk.yellow(`server on port ${port}`)));
